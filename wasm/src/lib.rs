dca77afadc71113d6d92b30867102cd98b717964
Alex Chan <alyxchan87@gmail.com>
2022-11-13 11:35:10 -0800

    Add wasm lib - WIP

---
+++ wasm/src/lib.rs
@@ -0,0 +1,88 @@
+use chip8_core::*;
+use wasm_bindgen::prelude::*;
+use web_sys::KeyboardEvent;
+
+#[wasm_bindgen]
+pub struct Chip8Wasm {
+    chip8: Chip8,
+}
+
+#[wasm_bindgen]
+impl Chip8Wasm {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Chip8Wasm {
+        Chip8Wasm {
+            chip8: Chip8::new(),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn tick(&mut self) {
+        self.chip8.tick();
+    }
+
+    #[wasm_bindgen]
+    pub fn tick_timers(&mut self) {
+        self.chip8.tick_timers();
+    }
+
+    #[wasm_bindgen]
+    pub fn reset(&mut self) {
+        self.chip8.reset();
+    }
+
+    #[wasm_bindgen]
+    pub fn keypress(&mut self, evt: KeyboardEvent, pressed: bool) {
+        let key = evt.key();
+        if let Some(k) = key2btn(&key) {
+            self.chip8.keypress(k, pressed);
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn load_game(&mut self, data: Uint8Array) {
+        self.chip8.load(&data.to_vec());
+    }
+
+    #[wasm_bindgen]
+    pub fn draw_screen(&mut self, scale: usize) {
+        // TODO
+    }
+}
+
+fn key2btn(key: &str) -> Option<usize> {
+    match key {
+        "1" => Some(0x1),
+        "2" => Some(0x2),
+        "3" => Some(0x3),
+        "4" => Some(0xC),
+        "q" => Some(0x4),
+        "w" => Some(0x5),
+        "e" => Some(0x6),
+        "r" => Some(0xD),
+        "a" => Some(0x7),
+        "s" => Some(0x8),
+        "d" => Some(0x9),
+        "f" => Some(0xE),
+        "z" => Some(0xA),
+        "x" => Some(0x0),
+        "c" => Some(0xB),
+        "v" => Some(0xF),
+        _ => None,
+    }
+}
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
